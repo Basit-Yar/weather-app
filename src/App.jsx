4561a5f05e864acc9a767e4408e3d3b8604dd291
SyedBasitIM <basityar6@gmail.com>
2025-01-03 22:01:18 +0530

    cleaning the default code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, saepe inventore velit voluptates voluptatibus earum nesciunt sint, esse maxime suscipit facere explicabo repudiandae neque aperiam recusandae quae praesentium quibusdam, voluptate illum qui! Eligendi, ducimus ad! Hic officiis fugiat eum cupiditate.</h1>
     </>
   )
 }
