028996ac603ab48e01da2b23f4e89e1ca2cc6f60
SyedBasitIM <basityar6@gmail.com>
2025-01-03 22:41:06 +0530

    header component initailization.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, saepe inventore velit voluptates voluptatibus earum nesciunt sint, esse maxime suscipit facere explicabo repudiandae neque aperiam recusandae quae praesentium quibusdam, voluptate illum qui! Eligendi, ducimus ad! Hic officiis fugiat eum cupiditate.</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const Header = () => {
+
+    return (
+        <>
+            <h1>This is a Header Section.</h1>
+        </>
+    )
+}
+
+export default Header;
