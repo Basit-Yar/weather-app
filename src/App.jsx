e8e41bbdfde9188f98540b19b1dc5f50fc448877
SyedBasitIM <basityar6@gmail.com>
2025-01-04 19:47:37 +0530

    adding layout components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className='text-red-600'>
-        This is something.
+      <div className='bg-green-100 w-full min-h-[100vh]'>
+        <Header />
+        <Footer />
       </div>
-      <Header />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import react from 'react';
+import { ChevronDown } from 'lucide-react';
 
 const Header = () => {
 
     return (
         <>
-            <h1>This is a Header Section.</h1>
+            <div className='h-16 shadow-md flex justify-between md:px-16 items-center'>
+                <div id='logo'>
+                    <h1 className='text-2xl font-extrabold text-violet-700'>
+                        WeatherForecast
+                    </h1>
+                </div>
+                <div id='theme-mode'
+                    className='flex items-end text-violet-700 font-semibold'
+                >
+                    <span className='text-lg'>Theme</span>
+                    <ChevronDown size={24} />
+                </div>
+            </div>
         </>
     )
 }
