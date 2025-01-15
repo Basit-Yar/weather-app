0d5db4980e7385836c2ab429df48c8feaade496d
SyedBasitIM <basityar6@gmail.com>
2025-01-15 18:59:23 +0530

    creating a new component for displaying today's weather forecast details.

--- src/components/WeatherLayout.jsx
+++ src/components/WeatherLayout.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import CurrentWeather from "./CurrentWeather";
+import TodayForecast from "./TodayForecast";
 
 const WeatherLayout = () => {
 
@@ -26,6 +27,7 @@ const WeatherLayout = () => {
     return (
         <>
             <CurrentWeather data={weatherData} />
+            <TodayForecast />
         </>
     )
 }
