bc125826ddaf72fde3697020d9e289a452697df0
SyedBasitIM <basityar6@gmail.com>
2025-01-21 12:48:56 +0530

    enhancing toggle switch btn.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,12 @@
 import { Search } from 'lucide-react'
+import { useState } from 'react';
 
 const SearchBar = () => {
 
+    const [isKelvin, setIsKelvin] = useState(true);
+    
+    const toggleSwitch = () => setIsKelvin(prev => !prev);
+    
     return (
         <>
             <div className="flex justify-between items-center p-3 sm:p-4 mx-3 sm:mx-16 my-4 rounded-md bg-green-100 shadow-md">
@@ -15,10 +20,12 @@ const SearchBar = () => {
                 {/* <div className='w-fit border border-green-700 rounded-md py-1 px-3 bg-white'>
                     Units
                 </div> */}
-                <div className='sm:w-24 w-16 h-6 sm:h-9 bg-white rounded-3xl flex justify-around items-center relative shadow-lg sm:text-base text-xs'>
+                <div className='sm:w-24 w-16 h-6 sm:h-9 bg-white rounded-3xl flex justify-around items-center relative border border-green-300 sm:text-base text-xs cursor-pointer'
+                    onClick={toggleSwitch}
+                >
                     <span className='z-10'>°C</span>
                     <span className='z-10'>°K</span>
-                    <div className='sm:w-10 w-7 sm:h-6 h-4 bg-green-300 rounded-2xl absolute right-1/2'></div>
+                    <div className={`sm:w-10 w-7 sm:h-6 h-4 bg-green-300 rounded-2xl absolute shadow-xl ${isKelvin ? 'right-1/2' : 'left-1/2'}`}></div>
                 </div>
             </div>
         </>
